88246d52852669bc51ec6bc85847c29d2b8112ca
Kalle Launiala <kalle.launiala@protonit.net>
2011-03-29 00:12:21 +0300

    Initial method body for copyBundleFileToDocumentsIfNotThereAlready

---
+++ CommonLib/CommonLibGenerator_v1_0.m
@@ -0,0 +1,24 @@
+﻿ 
+- (void)CopyBundleFileToDocumentsIfNotThereAlready:(NSString*) fileName
+{
+    //Get a fileManager object
+    NSFileManager *manager = [NSFileManager defaultManager];
+    
+    //Get the path to the desired data file in /Documents
+    NSString *docsDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *docsFilePath = [docsDir stringByAppendingPathComponent:fileName];
+    NSString *docsFileURL = [NSURL URLWithString:docsFilePath];
+    
+    //See if file is there already; if not, copy from bundle
+    if (![[NSFileManager defaultManager] fileExistsAtPath: docsFilePath]) {
+        
+        //Get the file URL to the file in the app bundle
+        // TODO: Fix code here! NOTE! Fix it in the template .tt file, not in the ".m" file
+        NSString* baseName = @"TODOBaseNameHere";
+        NSString* extensionName = @"TODOExtNameHere";
+        NSURL *bundleURL = [[NSBundle mainBundle] URLForResource:baseName withExtension:extensionName];
+        
+        //Copy the file from the bundle to the /Documents directory
+        [manager copyItemAtURL:bundleURL toURL:docsFileURL error:NULL];
+    }
+}
